576e72c979c91f950c755b0d400e5a2896181d5d
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2024-10-04 22:37:39 +0800

    Open code definition in new tab

--- lua/conf/settings.lua
+++ lua/conf/settings.lua
@@ -27,6 +27,6 @@ m.nmap("<leader>w", ":set wrap!<CR>")
 m.nmap("<C-T><C-W>", "<cmd>Telescope grep_string<CR>")
 -- cmp
 m.nmap("gD","<cmd>lua vim.lsp.buf.declaration()<CR>")
-m.nmap("gd","<cmd>lua vim.lsp.buf.definition()<CR>")
+m.nmap("gd","<cmd>tab split | lua vim.lsp.buf.definition()<CR>")
 m.nmap("K","<cmd>lua vim.lsp.buf.hover()<CR>")
 m.nmap("gs","<cmd>lua vim.lsp.buf.signature_help()<CR>")
