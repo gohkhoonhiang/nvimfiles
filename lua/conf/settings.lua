c2adc88c33a81610d248c107da910e11d1b36831
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2025-04-04 20:40:50 +0800

    Bind lsp function references

--- lua/conf/settings.lua
+++ lua/conf/settings.lua
@@ -28,5 +28,6 @@ m.nmap("<C-T><C-W>", "<cmd>Telescope grep_string<CR>")
 -- cmp
 m.nmap("gD","<cmd>lua vim.lsp.buf.declaration()<CR>")
 m.nmap("gd","<cmd>tab split | lua vim.lsp.buf.definition()<CR>")
+m.nmap("gf","<cmd>lua vim.lsp.buf.references()<CR>")
 m.nmap("K","<cmd>lua vim.lsp.buf.hover()<CR>")
 m.nmap("gs","<cmd>lua vim.lsp.buf.signature_help()<CR>")
