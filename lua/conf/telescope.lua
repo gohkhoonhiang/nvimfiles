7ed1dad8880ea8718c425fad513993e3cdbaa998
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2025-04-21 16:20:37 +0800

    Update telescope to not show hidden/gitignored files; add ignored patterns

--- lua/conf/telescope.lua
+++ lua/conf/telescope.lua
@@ -15,7 +15,7 @@ require("telescope").setup({
     },
     file_ignore_patterns = {
       "*.swp", "public/", "yarn.lock", ".gitignore", "node_modules/", "log/",
-      ".git/", "tmp/", "**/*.keep", ".ruby-lsp/", ".bundle/"
+      ".git/", "tmp/", "**/*.keep", ".ruby-lsp/", ".bundle/", "vendor/"
     },
     sorters = "fuzzy_with_index_bias",
   },
@@ -24,7 +24,7 @@ require("telescope").setup({
 -- Key bindings
 local m = require("conf.mappings")
 
-m.nmap("<C-P>", ":Telescope find_files previewer=false theme=get_dropdown hidden=true no_ignore=true<CR>")
+m.nmap("<C-P>", ":Telescope find_files previewer=false theme=get_dropdown<CR>")
 m.nmap("<C-S>", ":Telescope live_grep<CR>")
 m.nmap("<C-T><C-J>", ":Telescope jumplist<CR>")
 m.nmap("<C-T><C-G>", ":Telescope git_commits<CR>")
