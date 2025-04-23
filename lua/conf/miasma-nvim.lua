63f2904955e4c968cb24c6f64e7541c3bc240607
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2025-04-23 21:29:01 +0800

    Reconfigure miasma theme with indent highlight

--- lua/conf/miasma-nvim.lua
+++ lua/conf/miasma-nvim.lua
@@ -1,2 +1,8 @@
 vim.opt.termguicolors = true
+vim.api.nvim_create_autocmd({"ColorScheme"}, {
+ callback = function ()
+    vim.api.nvim_set_hl(0, "@ibl.scope.char.1", { fg = "#777777" })
+    vim.api.nvim_set_hl(0, "@ibl.indent.char.1", { fg = "#666666" })
+ end,
+})
 vim.cmd.colorscheme("miasma")
